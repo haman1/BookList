b8a567245099b9e4b57274214d1a508422a64598
ombasa <ombasaherman@gmail.com>
2021-11-27 23:33:37 +0300

    validation

--- main.js
+++ main.js
@@ -26,6 +26,26 @@ UI.prototype.addBookToList = function(book) {
     list.appendChild(row);
    
 }
+// show alert
+UI.prototype.showAlert = function(message, className) {
+    // Create div
+  const div = document.createElement('div');
+  // Add classes
+  div.className = `alert ${className}`;
+  // Add text
+  div.appendChild(document.createTextNode(message));
+  // Get parent
+  const container = document.querySelector('.container');
+  // Get form
+  const form = document.querySelector('#book-form');
+  // Insert alert
+  container.insertBefore(div, form);
+
+  // Timeout after 3 sec
+  setTimeout(function(){
+    document.querySelector('.alert').remove();
+  }, 3000);
+}
 //clear Fields
 UI.prototype.clearFields = function() {
     document.getElementById('title').value = '';
@@ -44,6 +64,20 @@ function(e) {
 
     const ui = new UI();
     console.log(ui);
+    //validate
+
+    if(title === '' || author === '' || isbn === '') {
+       //error alert
+       ui.showAlert('please fill in all fields', 'error');
+    } else {
+
+    //Add Book to list
+    ui.addBookToList(book);
+  
+    // clear fields
+    ui.clearFields();
+
+    }
     
     //Add Book to list
     ui.addBookToList(book);
